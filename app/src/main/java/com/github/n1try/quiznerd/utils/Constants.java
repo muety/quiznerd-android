00ab59f1d052c0eaf76bd120b916d7cc78af95dd
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2018-07-03 17:12:26 +0200

    Add basic ingame layout.

--- app/src/main/java/com/github/n1try/quiznerd/utils/Constants.java
+++ app/src/main/java/com/github/n1try/quiznerd/utils/Constants.java
@@ -1,6 +1,7 @@
 package com.github.n1try.quiznerd.utils;
 
 public final class Constants {
+    public static final int NUM_ROUNDS = 4;
     public static final String KEY_ME = "me";
     public static final String KEY_MATCH = "match";
 }
