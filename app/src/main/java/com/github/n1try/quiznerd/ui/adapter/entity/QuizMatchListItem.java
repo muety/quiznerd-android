e3d9ef2dd2114e249158d973eeb3adacbabacfd3
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2018-07-13 19:37:03 +0200

    Add match result indicator.

--- app/src/main/java/com/github/n1try/quiznerd/ui/adapter/entity/QuizMatchListItem.java
+++ app/src/main/java/com/github/n1try/quiznerd/ui/adapter/entity/QuizMatchListItem.java
@@ -46,6 +46,7 @@ public class QuizMatchListItem implements ListItem {
         TextView roundTv = convertView.findViewById(R.id.quiz_score_round_tv);
         TextView scoreTv = convertView.findViewById(R.id.quiz_score_tv);
         TextView turnTv = convertView.findViewById(R.id.quiz_turn_tv);
+        ImageView resultIv = convertView.findViewById(R.id.quiz_result_iv);
 
         int[] scores = match.getSortedScores(user);
         usernameTv.setText(match.getOpponent(user).getId());
@@ -55,9 +56,21 @@ public class QuizMatchListItem implements ListItem {
         if (match.isActive()) {
             roundTv.setVisibility(View.VISIBLE);
             scoreTv.setVisibility(View.GONE);
+            resultIv.setVisibility(View.GONE);
         } else {
             roundTv.setVisibility(View.GONE);
             scoreTv.setVisibility(View.VISIBLE);
+            resultIv.setVisibility(View.VISIBLE);
+            switch (match.getResult(user)) {
+                case WON:
+                    resultIv.setImageDrawable(context.getDrawable(R.drawable.ic_exposure_plus_1));
+                    break;
+                case LOST:
+                    resultIv.setImageDrawable(context.getDrawable(R.drawable.ic_exposure_neg_1));
+                    break;
+                default:
+                    resultIv.setImageDrawable(context.getDrawable(R.drawable.ic_exposure_zero));
+            }
         }
         categoryIv.setImageDrawable(QuizUtils.getCategoryIcon(context, match.getCategory()));
         if (!match.isMyTurn(user)) turnTv.setVisibility(View.GONE);
