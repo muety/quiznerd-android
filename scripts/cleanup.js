a3001cfec2fe8b834cc887259dcf9bbb78cfe928
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2020-02-06 10:43:54 +0100

    Update cleanup script to cover outdated matches and botdata.

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,53 +1,75 @@
 'use strict'
 
+// CAUTION: Don't forget to deactivate notifyMatchDeleted first!
 // Get credentials from https://console.cloud.google.com/iam-admin/serviceaccounts?project=quiznerd-49e4f&authuser=0
 
-const admin = require('firebase-admin'),
-    fs = require('fs');
+const admin = require('firebase-admin')
 
-const credentials = require('./data/quiznerd-49e4f-b97ae890548f.json');
+const credentials = require('./data/quiznerd-49e4f-c474d2fe3a83.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(credentials)
 });
 
+const dry = process.argv.includes('--dry')
+if (dry) console.log('Running in dry mode. Don\' worry...')
+
 let db = admin.firestore();
 let matches = db.collection('matches');
 let botdata = db.collection('botdata');
 
-let count1 = 0
-let count2 = 0
+let countMatches = 0
+let countBotdata = 0
 
-matches.where('active', '==', false)
-    .get()
-    .then(snapshot => {
-        let batch = db.batch()
-        snapshot.forEach(doc => {
-            count1++
-            batch.delete(doc.ref)
+let refDateMatches = new Date()
+let refDateBotdata = new Date()
+refDateMatches.setDate(refDateMatches.getDate() - 30)
+refDateBotdata.setDate(refDateBotdata.getDate() - 1)
+
+let matchPromises = [
+    matches.where('active', '==', false).get(), ,
+    matches.where('updated', '<=', refDateMatches).get()
+]
+
+let botdataPromises = [
+    botdata.where('active', '==', false).get(), ,
+    botdata.where('nextExecution', '<=', refDateBotdata).get()
+]
+
+Promise.all(matchPromises.filter(p => !!p))
+    .then(snapshots => {
+        let promises = snapshots.map(snapshot => {
+            let batch = db.batch()
+            snapshot.forEach(doc => {
+                countMatches++
+                if (!dry) batch.delete(doc.ref)
+            })
+            return batch.commit()
         })
-        return batch.commit()
+        return Promise.all(promises)
     })
     .then(() => {
-        console.log(`Successfully deleted ${count1} matches.`)
+        console.log(`Successfully deleted ${countMatches} matches.`)
     })
     .catch(err => {
-        console.log('Error getting matches', err);
-    });
+        console.log('Error deleting matches', err)
+    })
 
-botdata.where('active', '==', false)
-    .get()
-    .then(snapshot => {
-        let batch = db.batch()
-        snapshot.forEach(doc => {
-            count2++
-            batch.delete(doc.ref)
+Promise.all(botdataPromises.filter(p => !!p))
+    .then(snapshots => {
+        let promises = snapshots.map(snapshot => {
+            let batch = db.batch()
+            snapshot.forEach(doc => {
+                countBotdata++
+                if (!dry) batch.delete(doc.ref)
+            })
+            return batch.commit()
         })
-        return batch.commit()
+        return Promise.all(promises)
     })
     .then(() => {
-        console.log(`Successfully deleted ${count2} botdata documents.`)
+        console.log(`Successfully deleted ${countBotdata} botdata documents.`)
     })
     .catch(err => {
-        console.log('Error getting botdata documents', err);
-    });
+        console.log('Error deleting botdata', err)
+    })
